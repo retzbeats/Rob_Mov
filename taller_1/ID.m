0a4d668dd6d4163dbdf0c188e0049c7811b1749b
rojas-j <rojas-j@10.157.100.80>
2017-02-21 16:10:03 -0500

    All 3 points finished

--- taller_1/ID.m
+++ taller_1/ID.m
@@ -2,7 +2,7 @@ function [ tau ] = ID(SNAKE, PARAM, q, qd, qdd, grav, Fext)
 %ID Summary of this function goes here
 %   Detailed explanation goes here
 
-n, pt = size(q);
+[n, pt] = size(q);
 V = zeros(6, n+1);
 dV = zeros (6, n+1);
 V(:,1) = zeros(6,1);
@@ -26,26 +26,28 @@ for j=1:pt
         
         
         V(:,i+1) = R'*P*V(:,i)+H*qd(i,j);
-        dV(:,i+1) = R'*P*dV(:,i)+ W_skew_last*R'*P*V(:,i)+H*qdd(i,j)*W_skew_present*H*qd(i,j);
+        dV(:,i+1) = R'*P*dV(:,i)+ W_skew_last*R'*P*V(:,i)+H*qdd(i,j)+W_skew_present*H*qd(i,j);
     end
-    
-    for i=n:1
+    for k=1:n
+        i=n-k+1
         [r,p] = homogeneus_matrix(DH,q(i,j));
         R=[r, zeros(3,3);zeros(3,3),r];
         P=[eye(3) skew(p); zeros(3) eye(3)];
         J=[PARAM(i,5)  0 0; 0 PARAM(i,6) 0; 0 0 PARAM(i,7)];
-        I=[ J zeros(3); zeros(3), eye(3)*PARAM(i,1)];
-        s_skew=skew(PARAM(i,2:4))
+        I=[ J, zeros(3); zeros(3), eye(3)*PARAM(i,1)];
+        s_skew=skew(PARAM(i,2:4));
         S=[eye(3) s_skew; zeros(3) eye(3)];
         w_skew=skew(V(1:3,i));
-        W=[w_skew_present, zeros(3,3);
-                    zeros(3,3), w_skew_present];    
-        
-        F(:,i)=P'*R*F(:,i+1) + I*W*V(:,i) +(I*W*S + W*I)*V(:,i);
+        W=[w_skew, zeros(3,3);
+          zeros(3,3), w_skew];    
+        F(:,i)=P'*R*F(:,i+1) + I*dV(:,i) +(I*W*S + W*I)*V(:,i)
+        %a=P'*R*F(:,i+1)
+        %b=I*dV(:,i)
+        %c=(I*W*S + W*I)*V(:,i)
         tau(i,j) = H'*F(:,i);
     end
-    V(:,1) = V(:,2);
-    dV(:,1) = dV(:,2) + [0 0 0 grav]';
+    %V(:,1) = V(:,2);
+    %dV(:,1) = dV(:,2) + [0 0 0 grav]';
 end
 
 end
--- taller_1/main_t1.m
+++ taller_1/main_t1.m
@@ -1,6 +1,15 @@
+[SNAKE,PARAM]=snake_param();
 t = [0:0.1:9.9]; %con fi obtengo 100 posiciones (q);
-[p,d,d2,x,y]=serpenoid(100,pi/3,2*pi,1,3);
+[q,qd,qdd,x,y]=serpenoid(12,pi/3,2*pi,1,1);
 figure
 plot(t,p(1,:));
 figure
 plot (x,y);
+grav= [0,0,-9.81]
+id = ID (SNAKE,PARAM,q, qd, qdd,grav,0)
+figure
+for i=1:11
+    plot(t,id(i,:));
+    hold on
+end
+hold off
