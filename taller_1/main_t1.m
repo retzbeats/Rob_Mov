6196976ed75d364e2021f2fc430bfb51558798f3
Juan Guillermo Rojas Aguilar <rojas-j@javeriana.edu.co>
2017-02-21 18:58:14 -0500

    Serpenoid curves plot added

--- taller_1/main_t1.m
+++ taller_1/main_t1.m
@@ -1,4 +1,6 @@
 t = [0:0.1:9.9]; %con fi obtengo 100 posiciones (q);
-[p,d,d2]=serpenoid(12,2*pi/3,2*pi,0,3);
+[p,d,d2,x,y]=serpenoid(100,pi/3,2*pi,1,3);
 figure
 plot(t,p(1,:));
+figure
+plot (x,y);
--- taller_1/serpenoid.m
+++ taller_1/serpenoid.m
@@ -1,8 +1,19 @@
-function [q,qd,qdd] = serpenoid (n,a,b,c,w)
+function [q,qd,qdd,x,y] = serpenoid (n,a,b,c,w)
 t = [0:0.1:9.9]; %con fi obtengo 100 posiciones (q);
 beta = b/n;
 gama = -c/n;
 alpha = a*abs(sin(beta/2));
+x(1) = 0;
+y(1) = 0;
+
+for ind=1:n
+    for k=1:ind
+    x(ind+1) = 1/n * cos(a*cos(k*b/n) + k*c/n)+x(ind);
+    y(ind+1)= 1/n * sin(a*cos(k*b/n)+ k*c/n)+y(ind);
+    end
+end
+    
+
 for i=1:n-1
     for j=1:length(t)
     q(i,j) = alpha*sin(w*t(j)+(i-1)*beta)+gama;
